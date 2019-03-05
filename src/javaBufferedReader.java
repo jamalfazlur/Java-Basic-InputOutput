f2cc1e3640d4a782ab339e2d9d2f5e2f648a84df
Jamal Fazlur Rahman <jamalfazlurrahman@DOKUs-iMac-2.local>
2019-03-05 15:48:07 +0700

    Komit java pertama

---
+++ src/javaBufferedReader.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class javaBufferedReader {
+    public static void hitungBuffered(){
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(isr);
+        int value1 = 0;
+        int value2 = 0;
+        System.out.println("Program sederhana menghitung");
+        try {
+            System.out.println("Masukkan Angka Pertama: ");
+            value1 = Integer.parseInt(bufferedReader.readLine());
+            System.out.println("Masukkan Angka Kedua: ");
+            value2 = Integer.parseInt(bufferedReader.readLine());
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        int hasil = value1 + value2;
+        System.out.println("Hasilnya: " + hasil);
+    }
+}
---
+++ src/javaScanner.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class javaScanner {
+    public static void hitungScanner(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Program penjumlahan sederhana");
+        System.out.println("Masukkan Angka Pertama: ");
+        int value1 = scanner.nextInt();
+        System.out.println("Masukkan Angka Kedua: ");
+        int value2 = scanner.nextInt();
+        int hasil = value1 + value2;
+        System.out.println("Hasil Penjumlahan: " + hasil);
+    }
+}
